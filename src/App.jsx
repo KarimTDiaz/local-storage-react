7b6eb20b9a31f1ccee3340bd59b45c338a76cb13
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-05-31 21:36:45 +0200

    local storage - tester

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
-	return 
+	const [counter, setCounter] = useState(getInitialValue);
+	const [username, setUsername] = useState(getUserName);
+
+	useEffect(() => {
+		localStorage.setItem('data', JSON.stringify({ counter, username }));
+	}, [counter, username]);
+
+	return (
+		<>
+			<h1>{counter}</h1>
+			<h2>{username}</h2>
+			<input type='text' onChange={e => handleChange(e, setUsername)} />
+			<button onClick={() => handleIncrement(counter, setCounter)}>+1</button>
+		</>
+	);
 };
 
+const handleIncrement = (counter, setCounter) => {
+	setCounter(counter + 1);
+};
+const handleChange = (e, setUsername) => {
+	setUsername(e.target.value);
+};
+const getInitialValue = () => {
+	const initialValue = JSON.parse(localStorage.getItem('data'));
+	if (!initialValue) localStorage.setItem('data', JSON.stringify({}));
+	return Number(initialValue.counter) || 0;
+};
+
+const getUserName = () => {
+	const initialValue = JSON.parse(localStorage.getItem('data'));
+	if (!initialValue) localStorage.setItem('data', JSON.stringify({}));
+	return initialValue.username || 'username';
+};
 export default App;
